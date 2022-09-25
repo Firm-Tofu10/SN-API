df6fbd4a6c13f4387dbde3fcbfa7187060690043
Bryson.Tofu <70546630+Firm-Tofu10@users.noreply.github.com>
2022-09-25 00:36:37 -0600

    Added to utils files
    may not need this using NoSQL day3 as a reference .

---
+++ utils/date.js
@@ -0,0 +1,23 @@
+const names = [
+	'Dave',
+	'Kail',
+	'Shell',
+	'Maine',
+	'Tail',
+	'Shell',
+	'Izzy',
+	'Krate'
+];
+
+const email = [
+	wdaahsmnnepwhkvikz@kvhrr.com,
+	lxa91660@cdfaq.com,
+	edp05014@cdfaq.com,
+	vim16390@nezid.com,
+	phu21274@xcoxc.com,
+	khf11712@cdfaq.com,
+	mlr04491@nezid.com,
+	pfr06707@nezid.com
+]
+
+const 
